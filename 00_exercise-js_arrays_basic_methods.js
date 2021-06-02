1852cf44713365c3bea7b9d35a4e4ff6ac19066b
Lau <lmarcos@LT-ACER-ISA.localdomain>
2021-06-02 14:30:59 +0200

    digitsumagain

--- 00_exercise-js_arrays_basic_methods.js
+++ 00_exercise-js_arrays_basic_methods.js
@@ -2,6 +2,64 @@
  * Solution of basic methods exercise
  */
 
+ const unsortedNumbers = [100, 1974, 56, 0, -1, 25, 7, 9, 15, 10];
+ const words = [
+   "Hello",
+   "Goodbye",
+   "AA",
+   "First",
+   "A",
+   "a",
+   "Second",
+   "b",
+   "Third",
+ ];
+ 
+ console.log("unsorted", unsortedNumbers);
+ 
+ unsortedNumbers.sort(function (a, b) {
+   return a - b;
+ });
+ 0
+ console.log("sorted", unsortedNumbers);
+ 
+ let numbersPrimes = [25, 7, 9, 15, 10];
+ 
+ numbersPrimes.forEach(function (value) {
+   console.log(checkPrime(value));
+ });
+ 
+ // for (number of numbersPrimes) console.log(checkPrime(number));
+ 
+ function checkPrime(number) {
+   if (number === 0 || number === 1) return false;
+ 
+   for (let i = 2; i <= Math.sqrt(number); i++) {
+     if (number % i === 0) return false;
+   }
+   return true;
+ }
+ 
+ function checkPalindrome(string) {
+   const result = string.split("").reverse().join("") === string;
+   return result;
+ }
+ 
+ //console.log(checkPalindrome("anna"));
+ 
+ const arrayOfNumbers = [1, 2, 3, 4, 5, 6];
+ 
+ function sumArray(arrayOfNumbers) {
+   let sum = 0;
+   for (num of arrayOfNumbers) {
+     sum += num;
+   }
+   return sum;
+ }
+ 
+ //console.log(sumArray(arrayOfNumbers));
+ 
+
 //PRIMES
 
  function checkPrime(number) {
@@ -25,9 +83,17 @@
 // DIGIT SUM 
 
 let number = 42;
-console.log(number.toString().split('').reduce(function(r, n) { 
-    return r + parseInt(n) }, 0));
+console.log(number.toString().split('').reduce(function(r, n) { return r + parseInt(n) }, 0));
 
-  //console.log(sumDigits(42));
-  
   //resultat 6
+
+//SECOND NUMBERS
+
+const someNumbers =[4,2,6,9,5]
+
+
+
+
+  console.log(secondNumbers([4,2,6,9,5]));
+=> second highest: 6
+=> second lowest: 4
