64eeb3f9f865950595cff9d145ef1991ff5a3755
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-02 10:07:29 +0200

    WIP step 2

--- 00_code-js_arrays_basic_methods.js
+++ 00_code-js_arrays_basic_methods.js
@@ -2,10 +2,16 @@
  * Code along Array split() join() indexOf() includes()
  */
 
+let result;
+
+const someWords = ["cappucino", "caffé", "te", "aranciata", "23", false];
+
+result = someWords.includes(23);
+/*
 const someWords = ["cappucino", "caffé", "te", "aranciata", 23, false];
 const someString = "cappucino;caffé;te;aranciata;23;false";
 
-let result = someWords.join(" ");
-result = someString.split(";");
-
+result = someWords.join();
+result = someString.split();
+*/
 console.log(result);
